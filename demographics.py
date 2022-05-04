e288ccbf50fc4ed9e1ce9cda8a1b66954684ee18
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-05-04 11:55:07 -0700

    created demographics.py
    allows us to pull demographic lists, still needs work tho

---
+++ demographics.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import numpy as np
+
+dems = ['User ID','Age','Gender','Height','Ethnicity','LanguagesSpoken','Handedness','Major/Minor']
+ #accepts the dems arg which is simply a list of demographics you want to pull, important*** also include the column with your user id's first
+def givedems(dems,dems_csv):
+    df = pd.read_csv(rf'raw-data/{dems_csv}.csv')
+    df = pd.DataFrame(df,columns =dems )
+    for i in range (1,len(dems)-1):
+        ages = df.groupby(dems[i])
+        print(ages.groups)
+
+
+loc = "Demographics"
+givedems(dems,loc)
+        
