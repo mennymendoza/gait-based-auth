62284899510820e229598a25bb7a5ad508d031ec
drprado123 <103281716+drprado123@users.noreply.github.com>
2022-05-25 20:02:37 -0700

    added testfunc to auth

--- auth.py
+++ auth.py
@@ -5,9 +5,25 @@ import random
 import cycledetection
 import sys
 
-feature.build_feature_dataset(300)
-test_path = "./testing-data/1_HandPhone_Accelerometer_(Samsung_S6)-testing-data.csv"
-df = pd.read_csv(test_path)
-first_row = df.iloc[0].to_numpy()
-training_path = "./training-data/1_HandPhone_Accelerometer_(Samsung_S6)-training-data.csv"
-print(feature.get_anomaly_score(first_row, training_path))
+
+def setup():
+    feature.build_feature_dataset(300)
+    test_path = "./testing-data/1_HandPhone_Accelerometer_(Samsung_S6)-testing-data.csv"
+    df = pd.read_csv(test_path)
+    first_row = df.iloc[0].to_numpy()
+    training_path = "./training-data/1_HandPhone_Accelerometer_(Samsung_S6)-training-data.csv"
+    print(feature.get_anomaly_score(first_row, training_path))
+
+def testfunc():
+    feature.build_feature_dataset(300)
+    test_path = rf"./testing-data/1_PocketPhone_Accelerometer_(Samsung_S6) -testing-data.csv"
+    df = pd.read_csv(test_path)
+    first_row = df.iloc[0].to_numpy()
+    training_path = rf"./training-data/1_PocketPhone_Accelerometer_(Samsung_S6) -training-data.csv"
+    print(feature.get_anomaly_score(first_row, training_path))
+    test_path = rf"./testing-data/47_PocketPhone_Accelerometer_(Samsung_S6)-testing-data.csv"
+    df = pd.read_csv(test_path)
+    first_row = df.iloc[0].to_numpy()
+    print(feature.get_anomaly_score(first_row,training_path))
+
+
